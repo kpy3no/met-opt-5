5327e6a8c60cb42af0a73f71953089e054756de4
%40e.kuzmin.2013 <e.kuzmin.2013%40stud.nstu.ru>
2019-04-02 21:11:27 +0700

    first commit

---
+++ mo_lab5/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace mo_lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(260, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "x*y +";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(163, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "x^2 + ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(67, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "x^3 + ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 14);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(51, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "-1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "y";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 89);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(51, 20);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "-10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "x";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(398, 14);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(51, 20);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Начальное приближение: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Точность (eps)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(299, 14);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(51, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "-1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Найти решение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(353, 14);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "y^2 + ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(79, 89);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(51, 20);
+            this.textBox7.TabIndex = 22;
+            this.textBox7.Text = "-10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(452, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 16);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "y^3";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(150, 89);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(51, 20);
+            this.textBox8.TabIndex = 24;
+            this.textBox8.Text = "0,001";
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(233, 54);
+            this.console.Multiline = true;
+            this.console.Name = "console";
+            this.console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.console.Size = new System.Drawing.Size(439, 180);
+            this.console.TabIndex = 26;
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(13, 240);
+            this.chart1.Name = "chart1";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Color = System.Drawing.Color.LightSeaGreen;
+            series11.Legend = "Legend1";
+            series11.Name = "Поиск решения";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Color = System.Drawing.Color.Red;
+            series12.Legend = "Legend1";
+            series12.Name = "Начальная точка и решение";
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(659, 300);
+            this.chart1.TabIndex = 27;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Метод Ньютона";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox console;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ mo_lab5/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mo_lab5
+{
+    public partial class Form1 : Form
+    {
+        double c1, c2, c3, c4, c5;
+        double x, y;
+        double eps;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double dx(double x, double y) //Производная по x
+        {
+            return ((c1 * 3 * x * x) + (c2 * 2 * x) + (c3 * y));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        double dy(double x, double y) //Производная по y
+        {
+            return ((c3 * x) + (c4 * 2 * y) + (c5 * 3 * y * y));
+        }
+
+        double ddxx(double x) //Две производные по x
+        {
+            return ((c1 * 6 * x) + (c2 * 2));
+        }
+
+        double ddyy(double y) //Две производные по y
+        {
+            return ((c4 * 2) + (c5 * 6 * y));
+        }
+
+        double det(double x, double y) //Определитель гессиана
+        {
+            return ((ddxx(x) * ddyy(y)) - (c3 * c3));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = 0, r = 0;
+            double g1, g2;
+            double prev_x, prev_y; //Предыдущие x, y
+            double grad; //Длина вектора градиента
+
+            c1 = Convert.ToDouble(textBox1.Text);
+            c2 = Convert.ToDouble(textBox2.Text);
+            c3 = Convert.ToDouble(textBox3.Text);
+            c4 = Convert.ToDouble(textBox4.Text);
+            c5 = Convert.ToDouble(textBox5.Text);
+
+            x = Convert.ToDouble(textBox6.Text);
+            y = Convert.ToDouble(textBox7.Text);
+            eps = Convert.ToDouble(textBox8.Text);
+
+
+            prev_x = x;
+            prev_y = y;
+
+
+            console.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            chart1.Series[0].Points.AddXY(x, y);
+            chart1.Series[1].Points.AddXY(x, y);
+
+            while (k != 1000)
+            {
+                k++;
+
+                g1 = dx(x, y);
+                g2 = dy(x, y);
+
+                r += 2;
+
+
+                grad = Math.Sqrt(g1 * g1 + g2 * g2);
+                //grad = 1;
+                if (grad < eps) //Если градиент меньше eps, прерываем цикл
+                {
+                    console.AppendText("Количество итераций: " + k + "\n");
+                    console.AppendText("Количество вычислений: " + r + "\n");
+                    console.AppendText("Предыдущая точка: (" + Convert.ToString(prev_x) + "; " + Convert.ToString(prev_y) + ")\n");
+                    /*
+                        if(det(x, y) > 0 && grad != 0)
+                        {
+                            console.AppendText("Локальный минимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                        }
+                        else if(det(x, y) < 0 && grad != 0)
+                        {
+                            console.AppendText("Локальный максимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                        }
+                        else if(det(x, y) > 0 && grad == 0)
+                        {
+                            console.AppendText("Глобальный минимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                        }
+                        else if(det(x, y) < 0 && grad == 0)
+                        {
+                            console.AppendText("Глобальный максимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                        }
+                        else
+                        {
+                            console.AppendText("Седловая точка: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                        }
+                        */
+
+                    if (det(x, y) > 0 && grad != 0)
+                    {
+                        console.AppendText("Локальный минимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                    }
+                    else if (det(x, y) < 0 && grad != 0)
+                    {
+                        console.AppendText("Локальный максимум: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                    }
+                    else
+                    {
+                        console.AppendText("Точка минимума: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                    }
+
+                    break;
+                }
+
+                prev_x = x;
+                prev_y = y;
+
+                //x = prev_x - ((ddyy(prev_y) * g1 - g2) / det(prev_x, prev_y));
+                //y = prev_y - ((-g1 + ddxx(prev_x) * g2) / det(prev_x, prev_y));
+                x = prev_x - ((ddyy(prev_y) * g1 - c3 * g2) / det(prev_x, prev_y)); //Вычисление новых значений x и y
+                y = prev_y - ((-c3 * g1 + ddxx(prev_x) * g2) / det(prev_x, prev_y));
+
+                chart1.Series[0].Points.Add(x, y);
+
+                if (k < 21)
+                {
+                    console.AppendText("Итерация: " + k + "\n");
+                    console.AppendText("Текущая точка: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                    console.AppendText("Гессиан функции: " + Convert.ToString(det(x, y)) + "\n");
+                    console.AppendText("\n");
+                }
+                
+
+                r += 2;
+            }
+
+            chart1.Series[0].Points.AddXY(x, y);
+            chart1.Series[1].Points.AddXY(x, y);
+
+            //chart1.Series[1].Points.AddXY(x, y - 1);
+
+            if (k == 1000)
+            {
+                console.AppendText("Количество итераций: " + k + "\n");
+                console.AppendText("Количество вычислений: " + r + "\n");
+                console.AppendText("Последняя найденная точка: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+                console.AppendText("Гессиан функции: " + Convert.ToString(det(x, y)) + "\n");
+            }
+
+            //console.AppendText("Точка минимума: (" + Convert.ToString(x) + "; " + Convert.ToString(y) + ")\n");
+        }
+    }
+}
